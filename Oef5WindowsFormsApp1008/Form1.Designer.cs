480f4080f586a31ca0804203e799bf041c16426c
Martijn Wauters <wautersmartijn@gmail.com>
2020-08-10 15:32:44 +0200

    Add project files.

---
+++ Oef5WindowsFormsApp1008/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Oef5WindowsFormsApp1008
+{
+    partial class Wedstrijdopstelling
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CbKeeper = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbVerd = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbRflank = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbLflank = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbSpits = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbBZ1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbBZ2 = new System.Windows.Forms.ComboBox();
+            this.btnKeeper = new System.Windows.Forms.Button();
+            this.btnVerd = new System.Windows.Forms.Button();
+            this.btnRflank = new System.Windows.Forms.Button();
+            this.btnLflank = new System.Windows.Forms.Button();
+            this.btnSpits = new System.Windows.Forms.Button();
+            this.btnSelectie = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CbKeeper
+            // 
+            this.CbKeeper.FormattingEnabled = true;
+            this.CbKeeper.Location = new System.Drawing.Point(612, 30);
+            this.CbKeeper.Name = "CbKeeper";
+            this.CbKeeper.Size = new System.Drawing.Size(121, 21);
+            this.CbKeeper.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(509, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Keeper";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(509, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Verdediger";
+            // 
+            // cbVerd
+            // 
+            this.cbVerd.FormattingEnabled = true;
+            this.cbVerd.Location = new System.Drawing.Point(612, 71);
+            this.cbVerd.Name = "cbVerd";
+            this.cbVerd.Size = new System.Drawing.Size(121, 21);
+            this.cbVerd.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(509, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rechterflank";
+            // 
+            // cbRflank
+            // 
+            this.cbRflank.FormattingEnabled = true;
+            this.cbRflank.Location = new System.Drawing.Point(612, 115);
+            this.cbRflank.Name = "cbRflank";
+            this.cbRflank.Size = new System.Drawing.Size(121, 21);
+            this.cbRflank.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(509, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Linkerflank";
+            // 
+            // cbLflank
+            // 
+            this.cbLflank.FormattingEnabled = true;
+            this.cbLflank.Location = new System.Drawing.Point(612, 160);
+            this.cbLflank.Name = "cbLflank";
+            this.cbLflank.Size = new System.Drawing.Size(121, 21);
+            this.cbLflank.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(509, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Spits";
+            // 
+            // cbSpits
+            // 
+            this.cbSpits.FormattingEnabled = true;
+            this.cbSpits.Location = new System.Drawing.Point(612, 208);
+            this.cbSpits.Name = "cbSpits";
+            this.cbSpits.Size = new System.Drawing.Size(121, 21);
+            this.cbSpits.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(509, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Bankzitter 1";
+            // 
+            // cbBZ1
+            // 
+            this.cbBZ1.FormattingEnabled = true;
+            this.cbBZ1.Location = new System.Drawing.Point(612, 257);
+            this.cbBZ1.Name = "cbBZ1";
+            this.cbBZ1.Size = new System.Drawing.Size(121, 21);
+            this.cbBZ1.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(509, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Bankzitter 2";
+            // 
+            // cbBZ2
+            // 
+            this.cbBZ2.FormattingEnabled = true;
+            this.cbBZ2.Location = new System.Drawing.Point(612, 307);
+            this.cbBZ2.Name = "cbBZ2";
+            this.cbBZ2.Size = new System.Drawing.Size(121, 21);
+            this.cbBZ2.TabIndex = 12;
+            // 
+            // btnKeeper
+            // 
+            this.btnKeeper.Location = new System.Drawing.Point(137, 30);
+            this.btnKeeper.Name = "btnKeeper";
+            this.btnKeeper.Size = new System.Drawing.Size(127, 33);
+            this.btnKeeper.TabIndex = 14;
+            this.btnKeeper.Text = "Keeper";
+            this.btnKeeper.UseVisualStyleBackColor = true;
+            // 
+            // btnVerd
+            // 
+            this.btnVerd.Location = new System.Drawing.Point(137, 102);
+            this.btnVerd.Name = "btnVerd";
+            this.btnVerd.Size = new System.Drawing.Size(127, 33);
+            this.btnVerd.TabIndex = 15;
+            this.btnVerd.Text = "Verdediger";
+            this.btnVerd.UseVisualStyleBackColor = true;
+            // 
+            // btnRflank
+            // 
+            this.btnRflank.Location = new System.Drawing.Point(12, 195);
+            this.btnRflank.Name = "btnRflank";
+            this.btnRflank.Size = new System.Drawing.Size(127, 33);
+            this.btnRflank.TabIndex = 16;
+            this.btnRflank.Text = "Rechterflank";
+            this.btnRflank.UseVisualStyleBackColor = true;
+            // 
+            // btnLflank
+            // 
+            this.btnLflank.Location = new System.Drawing.Point(260, 195);
+            this.btnLflank.Name = "btnLflank";
+            this.btnLflank.Size = new System.Drawing.Size(127, 33);
+            this.btnLflank.TabIndex = 17;
+            this.btnLflank.Text = "Linkerflank";
+            this.btnLflank.UseVisualStyleBackColor = true;
+            // 
+            // btnSpits
+            // 
+            this.btnSpits.Location = new System.Drawing.Point(137, 294);
+            this.btnSpits.Name = "btnSpits";
+            this.btnSpits.Size = new System.Drawing.Size(127, 33);
+            this.btnSpits.TabIndex = 18;
+            this.btnSpits.Text = "Spits";
+            this.btnSpits.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectie
+            // 
+            this.btnSelectie.Location = new System.Drawing.Point(512, 361);
+            this.btnSelectie.Name = "btnSelectie";
+            this.btnSelectie.Size = new System.Drawing.Size(221, 39);
+            this.btnSelectie.TabIndex = 19;
+            this.btnSelectie.Text = "Maak Selectie";
+            this.btnSelectie.UseVisualStyleBackColor = true;
+            // 
+            // Wedstrijdopstelling
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSelectie);
+            this.Controls.Add(this.btnSpits);
+            this.Controls.Add(this.btnLflank);
+            this.Controls.Add(this.btnRflank);
+            this.Controls.Add(this.btnVerd);
+            this.Controls.Add(this.btnKeeper);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbBZ2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbBZ1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbSpits);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbLflank);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbRflank);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbVerd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CbKeeper);
+            this.Name = "Wedstrijdopstelling";
+            this.Text = "Wedstrijdopstelling";
+            this.Load += new System.EventHandler(this.Wedstrijdopstelling_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CbKeeper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbVerd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbRflank;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbLflank;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbSpits;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbBZ1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbBZ2;
+        private System.Windows.Forms.Button btnKeeper;
+        private System.Windows.Forms.Button btnVerd;
+        private System.Windows.Forms.Button btnRflank;
+        private System.Windows.Forms.Button btnLflank;
+        private System.Windows.Forms.Button btnSpits;
+        private System.Windows.Forms.Button btnSelectie;
+    }
+}
+
